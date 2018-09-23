137d9b86047e9c9ae5d1643bd6978c65aaa69b72
shuiherng <S70768d83>
2018-09-23 15:50:48 +0800

    Added Person ID and associated validations Refactored Person class to use 'PersonProperty' enum variable to accommodate future modifications to person attribute list.

---
+++ src/main/java/seedu/address/model/person/ID.java
@@ -0,0 +1,40 @@
+package seedu.address.model.person;
+
+/**
+ * Represents a Person's ID in the address book.
+ * ID only increases, and a person is bound to their ID forever.
+ */
+public class ID {
+
+    // ID iterator
+    public static int idCounter = 0;
+
+    public static final String ID_VALIDATION_REGEX = "^p[0-9]+$";
+    public final String value;
+
+    /**
+     * Constructs a {@code ID}.
+     * @return A valid ID.
+     */
+    public ID() {
+        value = getNewID();
+    }
+    private static String getNewID(){
+        return new String("p"+Integer.toString(idCounter));
+    }
+
+    public static boolean isValidID(String test) { return test.matches(ID_VALIDATION_REGEX); }
+
+    @Override
+    public String toString() { return value; }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ID // instnaceof handles nulls
+                && value.equals(((ID) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() { return value.hashCode(); }
+}
