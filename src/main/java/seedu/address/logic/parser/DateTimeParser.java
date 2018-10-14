1d88814626ca40b7ce0570cc1ef85f0886ab21a1
kumuwu <zhanglizhi.relax@gmail.com>
2018-10-14 17:13:08 +0800

    just realize the previous commit didnt capture, hence redoing here add Prompt class and modify DateTimeParser to parse Date and Time separately

--- src/main/java/seedu/address/logic/parser/DateTimeParser.java
+++ src/main/java/seedu/address/logic/parser/DateTimeParser.java
@@ -3,50 +3,148 @@ package seedu.address.logic.parser;
 import java.util.Calendar;
 import java.util.List;
 
+
 import javafx.util.Pair;
+import seedu.address.model.ScheduleModel;
+import seedu.address.model.event.ScheduleEvent;
 
 public class DateTimeParser {
 
+    private ScheduleModel scheduleModel;
 
-    public Pair<Calendar, Calendar> parseDateTime(String dateTimeInput) {
-        Calendar currentTime = Calendar.getInstance();
-        Pair<Calendar, Calendar> resultantTime = getResultantTime(currentTime, dateTimeInput);
-        return resultantTime;
+    public DateTimeParser (ScheduleModel model) {
+        this.scheduleModel = model;
     }
 
-    private Pair<Calendar, Calendar> getResultantTime(Calendar currentTime, String dateTimeInput) {
+    public Pair<Calendar, Calendar> parseDateTime(String dateInput) {
+        Pair<Calendar, Calendar> resultantDateInterval = parseDate(dateInput);
+        Pair<Calendar, Calendar> resultantTimeSlot = parseTime(resultantDateInterval);
+        return resultantTimeSlot;
+    }
 
-        switch (dateTimeInput) {
+
+    private Pair<Calendar, Calendar> parseDate(String input) {
+        Calendar currentTime = Calendar.getInstance();
+        Pair<Calendar, Calendar> resultantDateInterval = getResultantDate(currentTime, input);
+        return resultantDateInterval;
+
+    }
+
+    private Pair<Calendar, Calendar> parseTime(Pair<Calendar, Calendar> resultantDateInterval) {
+        String timeSlot = promptForTimeSlot(resultantDateInterval);
+        // TO-DO
+        // now do the parsing from the string input to Pair<Calendar, Calendar>
+        // the final Pair of Calendar should be ready to be put into a ScheduleEvent;
+        return new Pair<Calendar, Calendar>(Calendar.getInstance(), Calendar.getInstance()); // dummy
+    }
+
+    private String promptForTimeSlot(Pair<Calendar, Calendar> resultantDateInterval) {
+        String availableTime = getAvailableTimeBetween(resultantDateInterval);
+        Prompt prompt = new Prompt();
+        return prompt.promptForMoreInput(availableTime);
+    }
+
+    private String getAvailableTimeBetween(Pair<Calendar, Calendar> resultantDateInterval) {
+        scheduleModel.updateFilteredEventList((scheduleEvent) -> {
+            return scheduleEvent.getDate().getKey().after(resultantDateInterval.getKey())
+                    && scheduleEvent.getDate().getValue().before(resultantDateInterval.getValue());
+        });
+        // after and before here are strictly
+        // this probably means I need to set my date start time at 8:59 and end at 18:01
+        List<ScheduleEvent> scheduledAppointments = scheduleModel.getFilteredEventList();
+        StringBuilder availableTime = new StringBuilder();
+        // TO-DO
+        // now need to find the complement of scheduledAppointments;
+        return availableTime.toString();
+    }
+
+    private Pair<Calendar, Calendar> getResultantDate(Calendar currentTime, String dateInput) {
+
+        if (dateInput.startsWith("in")) {
+            return parseIn(currentTime, dateInput);
+        } else if (dateInput.startsWith("this") || dateInput.startsWith("next")) {
+            return parseThisOrNext(currentTime, dateInput);
+        }
+        switch (dateInput) {
         case "tomorrow":
         case "tmr":
-            return getSingleDateTime(currentTime, 1);
+            return getSingleDate(currentTime, 1);
         case "the day after tomorrow":
         case "the day after tmr":
-        case "in two days":
-        case "in 2 days":
-            return getSingleDateTime(currentTime, 2);
-        case "in three days":
-        case "in 3 days":
-            return getSingleDateTime(currentTime, 3);
-        case "next week":
-            return getNextWeekDateTime(currentTime);
-        case "next month":
-            return getNextMonthDateTime(currentTime);
-        case "in a few days":
+            return getSingleDate(currentTime, 2);
         case "recently":
         case "soon":
-            return getNearFutureDateTime(currentTime);
+            return getNearFutureDates(currentTime);
         default:
-            return getDateTimeFromSpecified(dateTimeInput); // user actually inputs the date (eg. 13/12/2018)
+            return getDateFromSpecified(dateInput); // user actually inputs the date (eg. 13/12/2018)
         }
+
     }
 
-    private Pair<Calendar, Calendar> getSingleDateTime(Calendar date, int offset) {
-        date.add(Calendar.DATE, offset);
-        return promptForTime(date, date);
+    private Pair<Calendar, Calendar> parseThisOrNext(Calendar currentTime, String dateInput) {
+        String[] splitString = dateInput.split("\\s+");
+        assert splitString[0].equals("next") || splitString[0].equals("in");
+        switch (splitString[1]) {
+        case "week":
+            if (splitString[0].equals("next")) {
+                return getWeekDates(currentTime, 1);
+            } else {
+                return getWeekDates(currentTime, 0);
+            }
+        case "month":
+            if (splitString[0].equals("next")) {
+                return getMonthDates(currentTime, 1);
+            } else {
+                return getMonthDates(currentTime, 0);
+            }
+
+        }
+
+        return null;
     }
 
-    private Pair<Calendar, Calendar> getNextWeekDateTime(Calendar date) {
+    private Pair<Calendar, Calendar> parseIn(Calendar currentTime, String dateInput) {
+        String[] splitString = dateInput.split("\\s+");
+        assert splitString[0].equals("in");
+        if (Character.isDigit(splitString[1].charAt(0))) {
+            int offset = Integer.parseInt(splitString[1]);
+            switch (splitString[2]) {
+            case "days":
+            case "day":
+                return getSingleDate(currentTime, offset);
+            case "weeks":
+            case "week":
+                return getWeekDates(currentTime, offset);
+            case "months":
+            case "month":
+                return getMonthDates(currentTime, offset);
+            }
+        } else if (dateInput.equals("in a few days")) {
+            return getNearFutureDates(currentTime);
+        } else {
+            return null;
+        }
+
+    }
+
+    private Pair<Calendar, Calendar> getSingleDate(Calendar currentDate, int offset) {
+        Calendar dateStart = (Calendar) currentDate.clone();
+        Calendar dateEnd = (Calendar) currentDate.clone();
+        dateStart.add(Calendar.DATE, offset);
+        dateEnd.add(Calendar.DATE, offset);
+        setDateStartAndEnd(dateStart, dateEnd);
+        Pair<Calendar, Calendar> date = new Pair<>(dateStart, dateEnd);
+        return date;
+    }
+
+    private void setDateStartAndEnd(Calendar start, Calendar end) {
+        start.set(Calendar.HOUR, 8);
+        start.set(Calendar.MINUTE, 59);
+        end.set(Calendar.HOUR, 18);
+        end.set(Calendar.MINUTE, 1);
+    }
+
+    private Pair<Calendar, Calendar> getWeekDates(Calendar date, int offset) {
         // TO-DO
         // get the next week start date and end date
 
@@ -57,7 +155,7 @@ public class DateTimeParser {
 
     }
 
-    private Pair<Calendar, Calendar> getNextMonthDateTime(Calendar date) {
+    private Pair<Calendar, Calendar> getMonthDates(Calendar date, int offset) {
         // TO-DO
         // get the next month start date and end date
 
@@ -67,15 +165,17 @@ public class DateTimeParser {
         return promptForTime(startDate, endDate);
     }
 
-    private Pair<Calendar, Calendar> getNearFutureDateTime(Calendar date) {
-        Calendar copy = (Calendar) date.clone();
-        date.add(Calendar.DATE, 1);
-        copy.add(Calendar.DATE, 7);
-
-        return promptForTime(date, copy);
+    private Pair<Calendar, Calendar> getNearFutureDates(Calendar currentDate) {
+        Calendar startDate = (Calendar) currentDate.clone();
+        Calendar endDate = (Calendar) currentDate.clone();
+        startDate.add(Calendar.DATE, 1);
+        endDate.add(Calendar.DATE, 7);
+        setDateStartAndEnd(startDate, endDate);
+        Pair<Calendar, Calendar> dateDuration = new Pair<>(startDate, endDate);
+        return dateDuration;
     }
 
-    private Pair<Calendar, Calendar> getDateTimeFromSpecified(String dateTimeInput) {
+    private Pair<Calendar, Calendar> getDateFromSpecified(String dateTimeInput) {
         // TO-DO
         // check if it is really a valid specified date input
         // if yes, create a Calendar object from the specified date input from user
