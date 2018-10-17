db7a9ecdac6a66e443e92a602533a5cd546d7093
pratyushghosh <pratyushghosh@users.noreply.github.com>
2018-10-17 22:19:30 +0800

    Created working (but as-yet unlinked) version of Medical Database Search feature

---
+++ src/main/java/seedu/address/storage/DrugSearchUtility.java
@@ -0,0 +1,19 @@
+package seedu.address.storage;
+
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * For searching through "src/main/resources/storage/datasetForDrugs.csv"
+ * for drugs whose names contain a certain keyword, then
+ * returning pharmacological information about those drugs.
+ */
+
+public class DrugSearchUtility {
+    private static final String drugDatasetPath = "src/main/resources/storage/datasetForDrugs.csv";
+
+    public static void (String args[])
+}
