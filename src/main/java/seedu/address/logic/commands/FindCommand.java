ece7ea4a3b6863d42af8f6912806db47777132f1
lixinze777 <e0202539@u.nus.edu>
2018-10-27 18:58:57 +0800

    Implement auto switch feature

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -6,7 +6,10 @@ import static seedu.address.logic.parser.CmdTypeCliSyntax.*;
 import java.util.Arrays;
 import java.util.List;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.events.ui.SwitchToPatientEvent;
+import seedu.address.commons.events.ui.SwitchToScheduleEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBookModel;
@@ -63,10 +66,12 @@ public class FindCommand extends Command {
             String[] nameKeywords = searchString.split("\\s+");
 
             addressBookModel.updateFilteredPersonList(new MatchPersonPredicate(Arrays.asList(nameKeywords)));
+            EventsCenter.getInstance().post(new SwitchToPatientEvent());
             cmdResult = String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,
                     addressBookModel.getFilteredPersonList().size());
         } else if (this.cmdType.equals(CMDTYPE_APPOINTMENT)) {
             scheduleModel.updateFilteredEventList(new ScheduleEventMatchesPredicate(searchString));
+            EventsCenter.getInstance().post(new SwitchToScheduleEvent());
             cmdResult = String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW,
                     scheduleModel.getFilteredEventList().size());
         } else if (this.cmdType.equals(CMDTYPE_DISEASE)) {
