b2f43e53a07e0ad6f359c68516daa0ac38ea0f5e
kumuwu <zhanglizhi.relax@gmail.com>
2018-10-27 17:04:33 +0800

    fix more exceptions and some bugs

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,9 +7,10 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    public static final String MESSAGE_EVENTS_LISTED_OVERVIEW = "%1$d events listed!";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The patient index provided is invalid";
+    public static final String MESSAGE_PERSONS_MATCH_BY_NAME_FAIL = "No patient could be found matching name provided";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d patients listed!";
+    public static final String MESSAGE_EVENTS_LISTED_OVERVIEW = "%1$d appointments listed!";
     public static final String MESSAGE_SYMPTOMS_LISTED_OVERVIEW = "%1$d symptoms listed!";
 
 }
