f52f5913f2f7ab841bfcea7bb3ba7df30866d3a8
lixinze777 <e0202539@u.nus.edu>
2018-11-06 11:23:50 +0800

    travis

--- src/main/java/seedu/address/ui/PersonBrowsePanel.java
+++ src/main/java/seedu/address/ui/PersonBrowsePanel.java
@@ -3,6 +3,7 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
@@ -10,6 +11,9 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.Person;
 
+/**
+ * The person browse panel of the App
+ */
 public class PersonBrowsePanel extends UiPart<Region> {
 
     private static final String DEFAULT_MESSAGE = "You have not selected any patient";
@@ -22,7 +26,7 @@ public class PersonBrowsePanel extends UiPart<Region> {
     private Label name;
 
     @FXML
-    private Label ID;
+    private Label id;
 
     @FXML
     private Label phone;
@@ -40,17 +44,25 @@ public class PersonBrowsePanel extends UiPart<Region> {
         registerAsAnEventHandler(this);
     }
 
+    /**
+     *  load the default browse panel
+     */
     public void loadDefaultBrowse() {
         name.setText(DEFAULT_MESSAGE);
-        ID.setText(DEFAULT_MESSAGE);
+        id.setText(DEFAULT_MESSAGE);
         phone.setText(DEFAULT_MESSAGE);
         address.setText(DEFAULT_MESSAGE);
         email.setText(DEFAULT_MESSAGE);
     }
 
+    /**
+     * load the browse panel
+     *
+     * @param person
+     */
     private void loadBrowse(Person person) {
         name.setText(person.getName().toString());
-        ID.setText(person.getId().toString());
+        id.setText(person.getId().toString());
         phone.setText(person.getPhone().toString());
         address.setText(person.getAddress().toString());
         email.setText(person.getEmail().toString());
