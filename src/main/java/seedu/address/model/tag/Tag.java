07e9e14f07063fc26ca970f1c87139158829b729
kumuwu <zhanglizhi.relax@gmail.com>
2018-11-11 01:17:44 +0800

    fix tag length

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,8 +9,11 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_TAG_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String MESSAGE_TAG_CONSTRAINTS = "Tags should be alphanumeric and not more than 20 characters";
     public static final String TAG_VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_INVALID_TAG = "Invalid Tags! \n%1$s";
+    public static final int MAX_TAG_LENGTH = 20;
+
 
     public final String tagName;
 
@@ -29,7 +32,7 @@ public class Tag {
      * Returns true if a given string is a valid tag name.
      */
     public static boolean isValidTagName(String test) {
-        return test.matches(TAG_VALIDATION_REGEX);
+        return test.matches(TAG_VALIDATION_REGEX) && test.length() < MAX_TAG_LENGTH;
     }
 
     @Override
